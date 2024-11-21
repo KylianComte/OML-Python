c61c56745adb1bf31d9cba6e621a2841476bddd8
Martin Russo <martin.russo@etu.iut-tlse3.fr>
2024-11-21 11:28:45 +0100

    Exercice 1 : finito pipo en entier (3méthodes)

--- TP2_stat_regression_Vetu.py
+++ TP2_stat_regression_Vetu.py
@@ -7,10 +7,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from statistics import fmean, variance
-'''###############################################
+###############################################
 # Exercice 1 :Première méthode avec la matrice de covariance
 ###############################################
-temperature=np.array([3.0, 5.8, 4, 13.1, 14.1, 18.7, 22.0, 20.4, 17.5, 13.1, 8.6, 4.0])
+temperature=np.array([3.0, 5.3, 8.4, 13.1, 14.1, 18.7, 22.0, 20.4, 17.5, 13.1, 8.6, 4.0])
 precipitations=np.array([74,61,61,28,68,115,28,25,45,83,144,60])
 
 # Calcul de la covariance
@@ -22,7 +22,7 @@ print('la covariance de X et Y est =',matrice_cov[0][1])
 matrice_var=variance(temperature)
 
 # Tracé des points 
-plt.figure(1)
+'''plt.figure(1)
 plt.scatter(temperature,precipitations)
 a1=matrice_cov[0][1]/matrice_var
 
@@ -44,30 +44,45 @@ plt.plot(x_trace,a1*x_trace+b1,'red')
 plt.title('températures et parécipitations dans le Sud Ouest en 2010')
 plt.xlabel('Températures en °C')
 plt.ylabel('Précipitation en mm')
-plt.show()
-'''
+plt.show()'''
+
 
 ###################################################################
 # Exercice 1 :Deuxième méthode : utilisation de la pseudo inverse
 ##########################################################
 
-# Création de la N
+# Création de la matrice N
 N = np.vstack([temperature, np.ones(len(temperature))]).T
 
 # Calcul de la pseudoinverse en utilisant np.linalg.pinv
-???
-a2,b2=???
+a2,b2=np.linalg.pinv(N).dot(precipitations)
 print('la droite de régression a pour équation y=',a2,'x+',b2)
 
-
+# Tracé des points
+plt.figure(2)
+plt.scatter(temperature,precipitations)
+x_trace=np.linspace(min(temperature),max(temperature),100)
+plt.plot(x_trace,a2*x_trace+b2,'red')
+plt.title('températures et parécipitations dans le Sud Ouest en 2010')
+plt.xlabel('Températures en °C')
+plt.ylabel('Précipitation en mm')
+plt.show()
 
 ###################################################################
 # Exercice 1 :Troisième méthode : utilisation de la fonction np.linalg.lstsq de Python
 ##########################################################
-#a3,b3=np.linalg.lstsq(N,precipitations,rcond=None)[0]
-#print('la droite de régression a pour équation y=',a3,'x+',b3)
+a3,b3=np.linalg.lstsq(N,precipitations,rcond=None)[0]
+print('la droite de régression a pour équation y=',a3,'x+',b3)
 
-
+# Tracé des points
+plt.figure(3)
+plt.scatter(temperature,precipitations)
+x_trace=np.linspace(min(temperature),max(temperature),100)
+plt.plot(x_trace,a3*x_trace+b3,'red')
+plt.title('températures et parécipitations dans le Sud Ouest en 2010')
+plt.xlabel('Températures en °C')
+plt.ylabel('Précipitation en mm')
+plt.show()
 
 #####################
 # Exercice 2
